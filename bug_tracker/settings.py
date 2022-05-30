5a9f0e971500a9e55357e4ecbc696358a28e353a
lourivanrluz <lourivan-rodrigues@hotmail.com>
2022-05-30 17:33:19 -0300

    fix:deploy config

--- bug_tracker/settings.py
+++ bug_tracker/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost", "dj-capstone.herokuapp.com/"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "dj-capstone.herokuapp.com"]
 
 
 # Application definition
