151a5ef700453fbf1d65e70f53b933b1dbafb45b
Gabriel Azevedo <gmarquesazti@gmail.com>
2022-06-02 14:52:24 -0300

    fix: update tickets urls

--- tickets/urls.py
+++ tickets/urls.py
@@ -4,6 +4,6 @@ from django.urls import path
 from tickets.views import TicketView, TicketAddView
 
 urlpatterns = [
-    path("ticket/project/<str:project_id>/", TicketView.as_view()),
-    path("ticket/user/<str:ticket_id>/", TicketAddView.as_view()),
+    path("projects/<str:project_id>/tickets", TicketView.as_view()),
+    path("projects/<str:project_id>/tickets/<str:ticket_id>/", TicketAddView.as_view()),
 ]
