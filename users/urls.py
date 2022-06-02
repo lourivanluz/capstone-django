426c6fedb728b46e641b759fe78b77d25aadcaac
Gabriel Azevedo <gmarquesazti@gmail.com>
2022-06-02 18:51:15 -0300

    fix: update register route name

--- users/urls.py
+++ users/urls.py
@@ -4,6 +4,6 @@ from rest_framework.urls import path
 from users.views import UserView, loguin
 
 urlpatterns = [
-    path("register/", UserView.as_view()),
+    path("users/", UserView.as_view()),
     path("login/", loguin),
 ]
