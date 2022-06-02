0bb5149b6bb2e828b3e1a7063e23ee8c924a0c6f
Gabriel Azevedo <gmarquesazti@gmail.com>
2022-06-02 16:30:53 -0300

    fix: update comment response

--- comments/serializers.py
+++ comments/serializers.py
@@ -1,9 +1,12 @@
 from rest_framework import serializers
 
 from comments.models import Comments
+from users.serializers import UsersSerializer
 
 class CommentsSerializer(serializers.ModelSerializer):
+    user = UsersSerializer(read_only=True)
+
     class Meta:
         model = Comments
-        fields = ('id', 'ticket', 'timestamp', 'content')
+        fields = ('id', 'timestamp', 'content', 'user')
         read_only_fields = ('id', 'ticket', 'timestamp')
