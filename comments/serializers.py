0c7fce3f1a2e78f416f2b3a7d80948039a416087
Gabriel Azevedo <gmarquesazti@gmail.com>
2022-06-02 15:27:54 -0300

    fix: clean imports

--- comments/serializers.py
+++ comments/serializers.py
@@ -1,7 +1,6 @@
 from rest_framework import serializers
 
 from comments.models import Comments
-from users.serializers import UsersSerializer
 
 class CommentsSerializer(serializers.ModelSerializer):
     class Meta:
